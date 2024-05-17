725aba7a02cfd15933d10101e291bd53531fe0eb
Alejandro Tapia <alejandro_tapia@autotodo.com>
2024-05-17 11:50:25 -0600

    Creacion de la primera migracion
    Creacion de la carpeta y la clase de la migracion: Add-Migration Generos + Enter
    
    Actualizacion de la bd:
    Update-Database

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -10,7 +10,20 @@ namespace MinimalAPIPeliculas
 		{//configuraciones como usar sql server, connectionString(cadena de conexion que aputna hacia nuestra base),etc
 		}
 		//Y con esto ahora si, ya podemos decir que tenemos una entidad, ya que esta usando DbSet, su estructura sera Genero
-		public DbSet<Genero> Generos { get; set; }
-		//Sin embargo la tabla aun no se ha creado, porque falta hacer la migracion
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseSqlServer("YourConnectionStringHere",
+					sqlServerOptionsAction: sqlOptions =>
+					{
+						sqlOptions.EnableRetryOnFailure(
+							maxRetryCount: 5, // Número máximo de reintentos
+							maxRetryDelay: TimeSpan.FromSeconds(30), // Tiempo máximo de espera entre reintentos
+							errorNumbersToAdd: null); // Errores adicionales para considerar transitorios
+					});
+			}
+		}
+		//Sin embargo la tabla aun no se ha creado, porque falta hacer la migracion por comados
 	}
 }
